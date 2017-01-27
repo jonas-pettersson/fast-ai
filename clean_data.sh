4abcf4129dc7eee654b80df159a8c1a97ac5e075
Ubuntu <ubuntu@ip-172-31-33-185.us-west-2.compute.internal>
2017-01-27 22:10:30 +0000

    changes

--- clean_data.sh
+++ clean_data.sh
@@ -3,9 +3,9 @@
 cut -f2 -d'/' filenames.csv | cut -f1 -d'.' >> filenames_clean.csv
 sed -r 's/\s+//g' predicts.csv > predicts_clean.csv
 paste -d',' filenames_clean.csv predicts_clean.csv > submission_tmp.csv
-echo "id,label" > submission.csv
-sort -t',' -n submission_tmp.csv >> submission.csv
-rm submission_tmp.csv
-rm filenames_clean.csv
-rm predicts_clean.csv
+#echo "id,label" > submission.csv
+sort -t',' -n submission_tmp.csv > submission.csv
+#rm submission_tmp.csv
+#rm filenames_clean.csv
+#rm predicts_clean.csv
 head submission.csv
