3464082824de58dc72cfa9c459d2f33e0cc2da21
Ubuntu <ubuntu@ip-172-31-5-5.eu-west-1.compute.internal>
2017-03-26 21:32:13 +0000

    resnet50

--- scripts/clean_data.sh
+++ scripts/clean_data.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-cut -f2 -d'/' filenames.csv | cut -f1 -d'.' > filenames_clean.csv
-sed -r 's/\s+//g' predicts.csv > predicts_clean.csv
+if [ "$1" == "-h" ]; then
+    echo usage: $0 filenames predictions
+    exit 0
+fi
+
+if [ $# -ne 2 ]
+then
+    echo usage: $0 filenames predictions
+    exit 1
+elif ! [ -f $1 & -f $2 ]
+then
+    echo $1 or $2 is not a valid filename
+    echo usage: $0 filenames predictions
+    exit 1
+else
+    FILE_NAMES=$1
+    PREDICTS=$2
+fi
+
+cut -f2 -d'/' $FILE_NAMES | cut -f1 -d'.' > filenames_clean.csv
+sed -r 's/\s+//g' $PREDICTS > predicts_clean.csv
 paste -d',' filenames_clean.csv predicts_clean.csv > submission_tmp.csv
 #echo "id,label" > submission.csv
 sort -t',' -n submission_tmp.csv > submission.csv
-#rm submission_tmp.csv
-#rm filenames_clean.csv
-#rm predicts_clean.csv
+rm -i submission_tmp.csv
+rm -i filenames_clean.csv
+rm -i predicts_clean.csv
 head submission.csv
